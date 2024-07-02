17d0f28692e07165b589745d51e880244aa68a87
Claire Songhyun Lee <clairesonglee@users.noreply.github.com>
2024-07-02 02:01:05 -0500

    Save most uptodate version

--- Notebooks/generate_supernodes.py
+++ Notebooks/generate_supernodes.py
@@ -71,11 +71,55 @@ def create_coarse_data(output_path, event_dir, resolution):
         subedge_feat = edge_feat[:, edge_indices]
         node_indices = set(subedge_feat[0]).union(set(subedge_feat[1]))
         node_indices = torch.tensor(list(node_indices), dtype=torch.int64)
+        print("Node indices upon creation = ", node_indices)
+        node_indices = node_indices.cpu()
       else:
         subedge_feat = edge_feat[edge_indices]
       print("Feature = ", feature, "Subfeature size = ", subedge_feat.size())
       subedge_feats.append(subedge_feat)
-    node_indices = node_indices.cpu()
+
+    # Avoid out of index error by eliminating edge_index values > len(pid)
+    if subedge_feats[2].max() >= len(node_indices):
+      print("Error: `event.edge_index` contains out-of-bounds indices.")
+      print(f"Maximum index in `event.edge_index`: {subedge_feats[2].max()}")
+      print(f"Size of `mask` along dimension 0: {len(node_indices)}")
+
+      # Filter out columns with out-of-bounds indices
+      #valid_indices = (subedge_feats[2] < len(node_indices)).all(0)
+      edge_index = subedge_feats[2]
+      mask_size = len(node_indices)
+      valid_mask = torch.zeros(mask_size, dtype=torch.bool)
+      for col in range(edge_index.shape[1]):
+          val = edge_index[:, col]
+          if (val[0] > mask_size) or (val[1] > mask_size):
+            valid_mask[col] = False
+          else:
+            valid_mask[col] = True
+      valid_indices = [i for i, val in enumerate(valid_mask) if val == True]
+      print("valid mask size = ", len(valid_mask))
+      #print("valid indices = ", valid_indices)
+
+      for subedge_feat in subedge_feats:
+        print("BEFORE subedge feat and size = ", subedge_feat, len(subedge_feat))
+        print("MAX VS NODE IDX LEN", subedge_feat.max(), len(node_indices))
+        if subedge_feat.dim() > 1:
+          subedge_feat = subedge_feat[:, valid_indices]
+          #node_indices = set(subedge_feat[0]).union(set(subedge_feat[1]))
+          #node_indices = torch.tensor(list(node_indices), dtype=torch.int64)
+          node_indices = node_indices[valid_mask]
+          node_indices = node_indices.cpu()
+        else:
+          subedge_feat = subedge_feat[valid_indices]
+        print("AFTER subedge feat and size = ", subedge_feat, len(subedge_feat))
+      #event.edge_index = event.edge_index[:, valid_indices]
+      print("Size of valid indices array = ", len(valid_indices))
+      print("Size of node indices array = ", len(node_indices))
+      print("Max in edge index array = ", subedge_feats[2].max())
+      print("Torch Max in edge index array = ", torch.max(subedge_feats[2]))
+      #print("Valid indices = ", valid_indices)
+      print("Node indices = ", node_indices)
+      print("New node indices array = ", node_indices[valid_mask])
+      print("New size of node indices array = ", len(node_indices[valid_mask]))
     
     subedge_true_feats = []
     for feature in edge_true_feats:
@@ -85,7 +129,7 @@ def create_coarse_data(output_path, event_dir, resolution):
       subedge_true_feats.append(subedge_true_feat)
     
     # Apply mask to event graph features
-    node_feats = ['x', 'pid', 'hid', 'pt', 'cell_data']
+    node_feats = ['x', 'pid', 'hid', 'pt', 'cell_data'] # all have same x dim
     subnode_feats = []
     print("Node indices size = ", len(node_indices))
     for feature in node_feats:
@@ -109,7 +153,7 @@ def create_coarse_data(output_path, event_dir, resolution):
                    'edge_index': subedge_feats[2], \
                    'modulewise_true_edges': subedge_true_feats[0], \
                    'signal_true_edges': subedge_true_feats[1]}
-    filename = save_data(event, coarse_dict, output_path, filename)
+    #filename = save_data(event, coarse_dict, output_path, filename)
     # Count true instances in y and y_pid labels
     y = subedge_feats[0]
     y_pid = subedge_feats[1]
@@ -306,37 +350,30 @@ def test_dataloader(event):
 def main():
   # Set filepaths and initialize variables 
   input_path = "/data/FNAL/events/train/*"
-  #super_path = "/data/FNAL/processed/train/*"
-  #cluster_path = "/data/FNAL/processed/train/*"
-  #output_path = "/data/FNAL/coarse_events/10p-res/train/*"
-  output_path = "/data/FNAL/coarse_events/train/*"
+  #super_path = "/data/FNAL/processed/train/"
+  #cluster_path = "/data/FNAL/processed/train/"
+  output_path = "/data/FNAL/coarse_events/10p-res/train/"
+  #output_path = "/data/FNAL/coarse_events/train/"
 
-  #input_path = "/data/FNAL/events/test/*"
+  input_path = "/data/FNAL/events/test/*"
   #super_path = "/data/FNAL/processed_no_emb/test/*"
   #cluster_path = "/data/FNAL/processed/test/*"
-  #output_path = "/data/FNAL/coarse_events/10p-res/test/"
+  output_path = "/data/FNAL/coarse_events/10p-res/test/"
 
   #input_path = "/data/FNAL/events/val/*"
   #super_path = "/data/FNAL/processed_no_emb/val/*"
   #cluster_path = "/data/FNAL/processed/val/*"
-  #output_path = "/data/FNAL/coarse_events/25p-res/val/"
-  #output_path = "/data/FNAL/coarse_events/val/*"
+  #output_path = "/data/FNAL/coarse_events/10p-res/val/"
+  #output_path = "/data/FNAL/coarse_events/val/"
 
-  #'''
   event_dir = glob(input_path)
-  subevent_dir = glob(output_path)
-  y_stats(event_dir, subevent_dir)
-  '''
+  #subevent_dir = glob(output_path)
+  #y_stats(event_dir, subevent_dir)
+  
   resolution = 0.10
   data = create_coarse_data(output_path, event_dir, resolution)
-  visualize_data(input_path, super_path, cluster_path)
-
-  config_path = "/home/csl782/FNAL/HierarchicalGNN/Modules/gMRT/Configs/HGNN_GMM.yaml"
-  with open(config_path) as f:
-    hparams = yaml.load(f, Loader=yaml.FullLoader)
-    data = create_super_data(input_path, super_path, **hparams)
-  visualize_data(input_path, super_path, cluster_path)
-  '''
+  #visualize_data(input_path, super_path, cluster_path)
+  #'''
 main()
 
 
